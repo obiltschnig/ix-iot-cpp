b06892c4ce2857fad23b4bd6419e94428f428abe
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2018-03-26 09:46:13 +0200

    added Poco and Beast samples

---
+++ BeastCarriots/BeastCarriots.cpp
@@ -0,0 +1,92 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <cstdlib>
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std::literals::string_literals;
+namespace asio = boost::asio;
+using tcp = asio::ip::tcp;
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace pt = boost::property_tree;
+
+void sendTemperature(const std::string& apiKey, const std::string device, float temperature)
+{
+	const auto host = "api.carriots.com"s;
+	const auto port = 80;
+	const auto path = "/streams"s;
+
+	std::ostringstream jsonStream;
+	jsonStream <<
+        "{"
+            "\"protocol\": \"v2\","
+            "\"checksum\": \"\","
+            "\"device\": \"" << device << "\","
+            "\"at\": \"now\","
+            "\"data\": {"
+                "\"temperature\":" << temperature <<
+            "}"
+        "}";
+	std::string json{jsonStream.str()};
+
+	boost::asio::io_context ioc;
+	tcp::resolver resolver{ioc};
+	tcp::socket socket{ioc};
+
+	const auto resolved = resolver.resolve(host, std::to_string(port));
+	boost::asio::connect(socket, resolved.begin(), resolved.end());
+
+	http::request<http::string_body> req{http::verb::post, path, 11};
+	req.set(http::field::host, host);
+	req.set("carriots.apikey"s, apiKey);
+	req.set(http::field::content_type, "application/json"s);
+	req.body() = json;
+	req.prepare_payload();
+	http::write(socket, req);
+
+	beast::flat_buffer buffer;
+	http::response<http::string_body> res;
+	http::read(socket, buffer, res);
+
+	boost::system::error_code ec;
+	socket.shutdown(tcp::socket::shutdown_both, ec);
+
+	if (ec && ec != boost::system::errc::not_connected)
+		throw boost::system::system_error{ec};
+
+	if (res.result_int() != 200)
+	{
+		pt::ptree pt;
+		std::istringstream bstr(res.body());
+		pt::read_json(bstr, pt);
+		const auto& respPt = pt.get_child("response");
+  		const auto resp = respPt.get_value<std::string>();
+		throw std::runtime_error(resp);
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+    const auto apiKey{"ce3a2a76dbd871f0ef773e21bf79a49cae2f4d8a27cf463c9a0508cd92a6e5b5"s};
+    const auto device{"RaspberryPi2@obiltschnig.obiltschnig"s};
+
+	try
+	{
+    	sendTemperature(apiKey, device, 23);
+	}
+	catch (std::exception& exc)
+	{
+		std::cerr << exc.what() << std::endl;
+	}
+
+    return 0;
+}
---
+++ PocoCarriots/PocoCarriots.cpp
@@ -0,0 +1,68 @@
+#include <Poco/Net/HTTPSClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/URI.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std::literals::string_literals;
+using Poco::Net::HTTPRequest;
+using Poco::Net::HTTPResponse;
+using Poco::Net::HTTPMessage;
+using Poco::Dynamic::Var;
+namespace JSON = Poco::JSON;
+
+void sendTemperature(const std::string& apiKey, const std::string device, float temperature)
+{
+	const Poco::URI uri{"https://api.carriots.com/streams"s};
+
+	std::ostringstream jsonStream;
+	jsonStream <<
+        "{"
+            "\"protocol\": \"v2\","
+            "\"checksum\": \"\","
+            "\"device\": \"" << device << "\","
+            "\"at\": \"now\","
+            "\"data\": {"
+                "\"temperature\":" << temperature <<
+            "}"
+        "}";
+	const std::string json{jsonStream.str()};
+
+	Poco::Net::HTTPSClientSession session{uri.getHost(), uri.getPort()};
+	HTTPRequest request{HTTPRequest::HTTP_POST, uri.getPathEtc(), HTTPMessage::HTTP_1_1};
+	request.setContentType("application/json"s);
+	request.setContentLength(json.length());
+	request.set("carriots.apikey"s, apiKey);
+
+	session.sendRequest(request) << json;
+
+	HTTPResponse response;
+	std::istream& istr = session.receiveResponse(response);
+
+	JSON::Parser parser;
+	const auto var = parser.parse(istr);
+	const auto pObject = var.extract<JSON::Object::Ptr>();
+	const auto message = pObject->getValue<std::string>("response"s);
+	if (response.getStatus() != HTTPResponse::HTTP_OK)
+		throw Poco::IOException("Request failed"s, message);
+}
+
+int main(int argc, char** argv)
+{
+    const auto apiKey{"ce3a2a76dbd871f0ef773e21bf79a49cae2f4d8a27cf463c9a0508cd92a6e5b5"s};
+    const auto device{"RaspberryPi2@obiltschnig.obiltschnig"s};
+
+	try
+	{
+    	sendTemperature(apiKey, device, 23);
+	}
+	catch (Poco::Exception& exc)
+	{
+		std::cerr << exc.displayText() << std::endl;
+	}
+
+    return 0;
+}
